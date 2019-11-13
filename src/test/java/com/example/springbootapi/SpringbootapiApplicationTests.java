9c31a181d05a71e94abcf881b53a060e3a93d757
guilherme <guilhermearmelo@gmail.com>
2019-11-13 09:53:11 -0300

    Implementação das configurações do SwaggerUI + Downgrade do Springboot

--- src/test/java/com/example/springbootapi/SpringbootapiApplicationTests.java
+++ src/test/java/com/example/springbootapi/SpringbootapiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.springbootapi;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
